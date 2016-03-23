82a302650c1833eb147cc1657616e22e15a4fb77
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-03-23 14:29:15 -0700

    Create Customer class with ability to search for a customer by name

---
+++ lib/customer.rb
@@ -0,0 +1,30 @@
+class Customer
+	attr_reader :name
+
+	@@customers = []
+
+	def initialize(options = {})
+		@name = options[:name]
+		add_to_customers
+	end
+
+	def Customer.all
+		@@customers
+	end
+
+	def Customer.find_by_name(customer_name)
+		customer = @@customers.select {|customer| customer.name == customer_name}
+		return customer[0]
+	end
+
+	private
+
+	def add_to_customers
+		customer_name = @@customers.select {|customer| customer.name == self.name}
+		if customer_name != []
+			raise DuplicateCustomerError, "'#{self.name}' already exists."
+		else
+			@@customers << self
+		end
+	end
+end
